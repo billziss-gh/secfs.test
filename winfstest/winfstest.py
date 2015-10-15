c9a060cac8a145e7bf3e63510f307b2a9201537a
Bill Zissimopoulos <billziss@navimatics.com>
2015-10-15 00:30:50 -0700

    winfstest: fix tabs to spaces

--- winfstest/winfstest.py
+++ winfstest/winfstest.py
@@ -37,46 +37,46 @@ __all__ = ["testline", "testeval", "testdone", "uniqname", "fstest", "expect"]
 
 ntests = 0
 def testline(ok, diag = ""):
-	global ntests
-	ntests += 1
-	print "%sok %s%s%s" % ("" if ok else "not ", ntests, " - " if diag else "", diag)
+    global ntests
+    ntests += 1
+    print "%sok %s%s%s" % ("" if ok else "not ", ntests, " - " if diag else "", diag)
 def testeval(expr):
-	f = sys._getframe(1)
-	testline(eval(expr, f.f_globals, f.f_locals), expr)
+    f = sys._getframe(1)
+    testline(eval(expr, f.f_globals, f.f_locals), expr)
 def testdone():
-	global ntests
-	print "1..%s" % ntests
-	ntests = 0
+    global ntests
+    print "1..%s" % ntests
+    ntests = 0
 
 def uniqname():
-	return "%08x" % random.randint(1, 2 ** 32)
+    return "%08x" % random.randint(1, 2 ** 32)
 
 fstest_exe = os.path.splitext(os.path.realpath(__file__))[0] + ".exe"
 def fstest(cmd):
-	arg = cmd.split() if hasattr(cmd, "split") else list(cmd)
-	out = subprocess.check_output([fstest_exe] + arg, universal_newlines=True)
-	out = out.split("\n")
-	res = []
-	for l in out[1:]:
-		if not l:
-			continue
-		d = {}
-		res.append(d)
-		for p in l.split():
-			k, v = p.split("=", 2)
-			if v.startswith('"') and v.endswith('"') and len(v) >= 2:
-				v = v[1:-1]
-			else:
-				try:
-					v = int(v, 0)
-				except:
-					pass
-			d[k] = v
-	return out[0], res
+    arg = cmd.split() if hasattr(cmd, "split") else list(cmd)
+    out = subprocess.check_output([fstest_exe] + arg, universal_newlines=True)
+    out = out.split("\n")
+    res = []
+    for l in out[1:]:
+        if not l:
+            continue
+        d = {}
+        res.append(d)
+        for p in l.split():
+            k, v = p.split("=", 2)
+            if v.startswith('"') and v.endswith('"') and len(v) >= 2:
+                v = v[1:-1]
+            else:
+                try:
+                    v = int(v, 0)
+                except:
+                    pass
+            d[k] = v
+    return out[0], res
 def expect(exp, cmd):
-	err, res = fstest(cmd)
-	if str(exp) == err:
-		testline(1, "expect %s %s" % (exp, cmd))
-	else:
-		testline(0, "expect %s %s - got %s" % (exp, cmd, err))
-	return err, res
+    err, res = fstest(cmd)
+    if str(exp) == err:
+        testline(1, "expect %s %s" % (exp, cmd))
+    else:
+        testline(0, "expect %s %s - got %s" % (exp, cmd, err))
+    return err, res
